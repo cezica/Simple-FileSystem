4a8d7bb333ea2140659fe0a5528a683912e6df41
AvramescuRazvan <avramescu_razvan10@yahoo.ro>
2022-11-21 10:27:24 +0200

    Update fs.c

--- 3-cod/fs.c
+++ 3-cod/fs.c
@@ -67,44 +67,6 @@ void create_fs()
     create_disk_blocks();
 }
 
-int getNum(char ch)
-{
-    int num = 0;
-    if (ch >= '0' && ch <= '9') {
-        num = ch - 0x30;
-    }
-    else {
-        switch (ch) {
-        case 'A':
-        case 'a':
-            num = 10;
-            break;
-        case 'B':
-        case 'b':
-            num = 11;
-            break;
-        case 'C':
-        case 'c':
-            num = 12;
-            break;
-        case 'D':
-        case 'd':
-            num = 13;
-            break;
-        case 'E':
-        case 'e':
-            num = 14;
-            break;
-        case 'F':
-        case 'f':
-            num = 15;
-            break;
-        default:
-            num = 0;
-        }
-    }
-    return num;
-}
 
 unsigned int hex2int(unsigned char hex[])
 {
