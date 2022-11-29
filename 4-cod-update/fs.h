50de14f4bd1d93351db631b2c412c4194cf9cf34
cezica <103094958+cezica@users.noreply.github.com>
2022-11-29 19:38:35 +0200

    detalii adaugate

---
+++ 4-cod-update/fs.h
@@ -0,0 +1,66 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <stdint.h>
+
+#define DISK_BLOCKS_SIZE 64//momentan mica pentru testari
+#define MAX_NAME_FILE 255
+#define MAX_DISK_BLOCKS 100
+#define MAX_CHILDREN 10
+
+#define MAX_INODES 32
+
+#define MAX_READ 4096
+
+struct superblock{
+    //uint32_t magic; /* Magic number */
+
+    int nr_blocks; /* Total number of blocks (incl sb & inodes) */
+    int nr_inodes; /* Total number of inodes */
+
+    int nr_free_inodes; /* Number of free inodes */
+    int nr_free_blocks; /* Number of free blocks */
+
+    int* i_bitmap; //bitmap inodes
+    int* b_bitmap; //bitmap disk blocks
+
+    int current_dir;//index inode
+
+    //+padding
+};
+
+struct inode{
+    char name[MAX_NAME_FILE];
+    int i_parent;
+    int i_mode;   // File mode 
+    //int i_ctime;  //status changed
+    //int i_mtime;  //status modification
+    //int i_atime;  //status acces
+    int i_uid;    // Owner id 
+    int i_gid;    // Group id
+    int i_size;   // Size in bytes 
+    int i_blocks; // Blocks
+
+    //int i_children[MAX_CHILDREN]; //maxim de copii(fisiere+directoare)
+    //int* disk_blocks
+};
+
+struct disk_block{
+    unsigned char* data;
+};
+
+#define OFFSET (sizeof(struct superblock)+sizeof(struct inode)*MAX_INODES)
+
+struct file{
+    int inode;
+    char name[256];
+};
+
+void create_fs(); //initializare fs
+void mount_fs(); //incarcarea unui fs
+void sync_fc(); //scriere fs
